68eecbb682294d57dd36955ca61a1ffdf206d517
Mustafa Elsayed <zakatak82@gmail.com>
2026-05-13 18:32:26 -0400

    update meta pixel to dataset 1668366484189578

--- peptobuy/app/layout.tsx
+++ peptobuy/app/layout.tsx
@@ -72,11 +72,16 @@ export default function RootLayout({
           if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
           n.queue=[];t=b.createElement(e);t.async=!0;
           t.src=v;s=b.getElementsByTagName(e)[0];
-          s.parentNode.insertBefore(t,s)}(window,document,'script',
+          s.parentNode.insertBefore(t,s)}(window, document,'script',
           'https://connect.facebook.net/en_US/fbevents.js');
-          fbq('init','958503717098446');
-          fbq('track','PageView');
+          fbq('init', '1668366484189578');
+          fbq('track', 'PageView');
         `}</Script>
+        <noscript>
+          <img height="1" width="1" style={{display:"none"}}
+            src="https://www.facebook.com/tr?id=1668366484189578&ev=PageView&noscript=1"
+          />
+        </noscript>
       </head>
       <body className="bg-background text-foreground font-sans antialiased">
         <CartProvider>
