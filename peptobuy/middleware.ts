e16c0de6f291b6b5cfbf42c6aaf33528873ec71d
Mustafa Elsayed <zakatak82@gmail.com>
2026-06-23 14:04:56 -0400

    password protect entire site archive

---
+++ peptobuy/middleware.ts
@@ -0,0 +1,26 @@
+import { NextResponse } from 'next/server'
+import type { NextRequest } from 'next/server'
+
+export function middleware(request: NextRequest) {
+  const basicAuth = request.headers.get('authorization')
+
+  if (basicAuth) {
+    const authValue = basicAuth.split(' ')[1]
+    const [user, pwd] = Buffer.from(authValue, 'base64').toString().split(':')
+
+    if (user === 'admin' && pwd === process.env.SITE_PASSWORD) {
+      return NextResponse.next()
+    }
+  }
+
+  return new NextResponse('Authentication required', {
+    status: 401,
+    headers: {
+      'WWW-Authenticate': 'Basic realm="Secure Area"',
+    },
+  })
+}
+
+export const config = {
+  matcher: '/:path*',
+}
